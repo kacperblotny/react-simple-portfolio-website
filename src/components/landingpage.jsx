82cabfad7b0b3fb6a7300ba1dc4e287f76418356
Kacper <kacperblotny@192.168.0.103>
2023-01-10 14:07:49 +0100

    changes

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -4,27 +4,22 @@ import programming from '../assets/programming.svg'
 function landingpage() {
   return (
     <div className='flex h-screen w-screen pt-16 md:pt-32'>
-      <div className='mx-auto flex flex-col md:flex-row'>
-        <div className='my-auto w-full text-black dark:text-white md:w-96'>
-          <div className='m-w-fit'>
-            <p>Hello, my name is</p>
-            <p className='text-7xl'>Kacper Błotny</p>
-            <p className='text-7xl'>Web developer</p>
-            <p>
-              I'm a beginner software engineer currently specilizing in
-              front-end.
-            </p>
-          </div>
-          <div>
-            <a
-              href='#'
-              className='mt-8 flex h-min w-fit rounded bg-primary px-4 py-2 text-xl text-white duration-150 ease-in hover:scale-105 hover:text-white'
-            >
-              Read more about me!
-            </a>
-          </div>
+      <div className='mx-auto flex w-4/5 md:flex-row'>
+        <div className='my-auto text-black dark:text-white md:w-3/5'>
+          <p>Hello, my name is</p>
+          <p className='text-7xl'>Kacper Błotny</p>
+          <p className='text-7xl'>Web developer</p>
+          <p>
+            I'm a beginner software engineer currently specilizing in front-end.
+          </p>
+          <a
+            href='#'
+            className='mt-8 flex h-min w-fit rounded bg-primary px-4 py-2 text-xl text-white duration-150 ease-in hover:scale-105 hover:text-white'
+          >
+            Read more about me!
+          </a>
         </div>
-        <img src={programming} alt='' className='md:pl-12' />
+        <img src={programming} alt='' className='md:w-3/5 md:pl-12' />
       </div>
     </div>
   )
